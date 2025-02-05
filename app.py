be7d54f315c8548cf18a923558650e859afb160c
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-02-06 01:40:11 +0530

    "" missing

--- app.py
+++ app.py
@@ -479,4 +479,4 @@ def get_stats(username):
         return jsonify(asdict(error_response))
 
 if __name__ == '__main__':
-    app.run(host=0.0.0.0, debug=True)
+    app.run(host="0.0.0.0", debug=True)
